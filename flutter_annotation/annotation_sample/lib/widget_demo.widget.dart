dff9ac1673ba62e6f1fdd96c7cedbeb83c19c87d
yuhaocan@weidian.com <yuhaocan@weidian.com>
2021-01-07 23:50:24 +0800

    do：flutter annotation demo

---
+++ flutter_annotation/annotation_sample/lib/widget_demo.widget.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// WidgetMarkGenerator
+// **************************************************************************
+
+///class StatefulWidgetDemo extends StatefulWidget*
+class StatefulWidgetDemoMark {}
+
+///class StatelessWidgetDemo extends StatelessWidget*
+class StatelessWidgetDemoMark {}
